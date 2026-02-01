465ad6053971fd0a9a391545467776807bc72903
Data-N-Analysis <ysm10211@gmail.com>
2026-02-01 18:57:20 +0900

    refactor: provider 위치 변경 및 StrictMode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,5 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
 import './index.css';
 import App from './App.tsx';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(<App />);
