ccc28c84a5768e4e58b5f70ba6828754143f8d3b
Data-N-Analysis <ysm10211@gmail.com>
2026-02-01 19:06:57 +0900

    chore: Prettier 설정 변경 및 ESLint 주석 제거

--- eslint.config.js
+++ eslint.config.js
@@ -5,46 +5,41 @@ import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 
 export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{ts,tsx}'],
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    { ignores: ['dist'] },
+    {
+        files: ['**/*.{ts,tsx}'],
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        languageOptions: {
+            parser: tseslint.parser,
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+                ...globals.es2020,
+            },
+        },
+        rules: {
+            'no-undef': 'error',
+            'no-const-assign': 'error',
+            'no-duplicate-case': 'error',
+            'no-unused-vars': 'off',
+            'no-debugger': 'warn',
+
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+            'no-console': 'off',
+        },
     },
-    languageOptions: {
-      parser: tseslint.parser,
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.es2020,
-      },
-    },
-    rules: {
-      // [js.configs.recommended]
-      'no-undef': 'error',
-      'no-const-assign': 'error',
-      'no-duplicate-case': 'error',
-      'no-unused-vars': 'off',
-      'no-debugger': 'warn',
-
-      // [tseslint.configs.recommended]
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-non-null-assertion': 'off',
-
-      // [reactHooks.configs.flat.recommended]
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-
-      // [reactRefresh.configs.vite]
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-
-      // 기타
-      'no-console': 'off',
-    },
-  },
-  prettier,
+    prettier,
 ];
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,10 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 100,
-  tabWidth: 2,
-  arrowParens: 'always',
-  plugins: ['prettier-plugin-tailwindcss'],
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 100,
+    tabWidth: 4,
+    arrowParens: 'always',
+    plugins: ['prettier-plugin-tailwindcss'],
 };
